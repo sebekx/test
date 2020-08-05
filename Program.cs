bbbc15efadb21c6dcb5a4749c5192f9f06eb5723
Sebastian Zederowski <szederowski@future-processing.com>
2020-08-05 11:32:46 +0200

    test

--- Program.cs
+++ Program.cs
@@ -7,6 +7,11 @@ namespace openshift_test
         static void Main(string[] args)
         {
             Console.WriteLine("Hello World!");
+
+            System.IO.File.WriteAllText("/data/test.txt", "Test");
+
+            Console.WriteLine(System.IO.File.ReadAllText("/data/test.txt"));
+            Console.WriteLine("END!");
         }
     }
 }
